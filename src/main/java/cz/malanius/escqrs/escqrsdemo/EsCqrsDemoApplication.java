d4baabb39c4857d203ebf0b86b160d4c9f99e909
Malanius Privierre <malaniusprivierre@gmail.com>
2021-01-19 14:59:27 +0100

    Initial commit

---
+++ src/main/java/cz/malanius/escqrs/escqrsdemo/EsCqrsDemoApplication.java
@@ -0,0 +1,13 @@
+package cz.malanius.escqrs.escqrsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsCqrsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsCqrsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/malanius/escqrs/escqrsdemo/EsCqrsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.malanius.escqrs.escqrsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsCqrsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

495a0518996317974f497f53e271a3d38b336045
Malanius Privierre <malaniusprivierre@gmail.com>
2021-01-19 15:15:37 +0100

    Create data model

---
+++ src/main/java/cz/malanius/escqrs/escqrsdemo/model/Address.java
@@ -0,0 +1,14 @@
+package cz.malanius.escqrs.escqrsdemo.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Address {
+
+    private String city;
+    private String state;
+    private String postcode;
+
+}

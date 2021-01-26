d190de90b01eb46ebf7d06ab02394a6dc80bdd7c
Malanius Privierre <malaniusprivierre@gmail.com>
2021-01-26 15:48:20 +0100

    Prepare hydration of user

--- src/main/java/cz/malanius/escqrs/escqrsdemo/store/EventStore.java
+++ src/main/java/cz/malanius/escqrs/escqrsdemo/store/EventStore.java
@@ -5,10 +5,7 @@ import cz.malanius.escqrs.escqrsdemo.events.Event;
 import lombok.Getter;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 // In-memory for demo purposes is enough
 @Service
@@ -17,4 +14,13 @@ public class EventStore {
     @Getter
     private final Map<UUID, List<Event>> store = new HashMap<>();
 
+    public void addEvent(UUID userId, Event event) {
+        List<Event> events = Optional.ofNullable(store.get(userId)).orElse(new ArrayList<>());
+        events.add(event);
+        store.put(userId, events);
+    }
+
+    public List<Event> getEventsForUser(UUID userId) {
+        return List.copyOf(store.get(userId));
+    }
 }

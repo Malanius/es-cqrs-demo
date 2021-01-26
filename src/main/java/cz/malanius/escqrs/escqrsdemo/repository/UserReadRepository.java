999daab3f3931b94c73b39b79e3f355e1458b16a
Malanius Privierre <malaniusprivierre@gmail.com>
2021-01-26 12:28:29 +0100

    Create in-memory user read repository

---
+++ src/main/java/cz/malanius/escqrs/escqrsdemo/repository/UserReadRepository.java
@@ -0,0 +1,17 @@
+package cz.malanius.escqrs.escqrsdemo.repository;
+
+import cz.malanius.escqrs.escqrsdemo.projections.UserAddresses;
+import cz.malanius.escqrs.escqrsdemo.projections.UserContacts;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
+
+// For demo purposes, in-memory only
+@Data
+@Component
+public class UserReadRepository {
+    private Map<UUID, UserAddresses> userAddresses;
+    private Map<UUID, UserContacts> userContacts;
+}
